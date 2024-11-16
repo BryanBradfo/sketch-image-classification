b727fa0b65816c2cb849aa857887d5e61744dd08
BryanBradfo <bryanchen1105@gmail.com>
2024-11-16 21:11:49 +0100

    back to no transformation

--- data.py
+++ data.py
@@ -3,13 +3,13 @@ import torchvision.transforms as transforms
 # once the images are loaded, how do we pre-process them before being passed into the network
 # by default, we resize the images to 64 x 64 in size
 # and normalize them to mean = 0 and standard-deviation = 1 based on statistics collected from ImageNet
-# data_transforms = transforms.Compose(
-#     [
-#         transforms.Resize((64, 64)),
-#         transforms.ToTensor(),
-#         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-#     ]
-# )
+data_transforms = transforms.Compose(
+    [
+        transforms.Resize((64, 64)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
 
 from torchvision import transforms
 
@@ -29,15 +29,15 @@ from torchvision import transforms
 #     ),
 # ])
 
-data_transforms = transforms.Compose([
-    transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomVerticalFlip(),
-    transforms.RandomRotation(15),
-    transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
+# data_transforms = transforms.Compose([
+#     transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),
+#     transforms.RandomHorizontalFlip(),
+#     transforms.RandomVerticalFlip(),
+#     transforms.RandomRotation(15),
+#     transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+# ])
 
 # data_transforms = transforms.Compose([
 #     transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),
