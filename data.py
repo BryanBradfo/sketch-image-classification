786d1f36da579c222eb54e2c35ac1672b40946af
BryanBradfo <bryanchen1105@gmail.com>
2024-11-17 10:02:37 +0100

    add of randomerasing of 0.2

--- data.py
+++ data.py
@@ -38,6 +38,7 @@ data_transforms = transforms.Compose([
     transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.RandomErasing(p=0.2)
 ])
 
 # data_transforms = transforms.Compose([
