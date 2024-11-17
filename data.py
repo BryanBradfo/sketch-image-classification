de36deb93669fb0e6923e20ab66e99f37099d211
BryanBradfo <bryanchen1105@gmail.com>
2024-11-17 15:57:17 +0100

    gaussian blur

--- data.py
+++ data.py
@@ -35,6 +35,7 @@ data_transforms = transforms.Compose([
     transforms.RandomVerticalFlip(),
     transforms.RandomRotation(15),
     transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.8, 1.2)),
+    transforms.GaussianBlur(kernel_size=3),
     transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
