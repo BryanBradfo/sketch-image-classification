7d3875c1806d6d5ffd90d4fbd4bb41367ab72b30
BryanBradfo <bryanchen1105@gmail.com>
2024-11-18 18:25:01 +0100

    add of another model

--- data.py
+++ data.py
@@ -29,17 +29,21 @@ from torchvision import transforms
 #     ),
 # ])
 
+from torchvision.transforms import AutoAugment, AutoAugmentPolicy
+
+
 data_transforms = transforms.Compose([
     transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),
     transforms.RandomHorizontalFlip(),
     transforms.RandomVerticalFlip(),
     transforms.RandomRotation(15),
-    transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.8, 1.2)),
-    transforms.GaussianBlur(kernel_size=3),
+    AutoAugment(policy=AutoAugmentPolicy.IMAGENET),
+    # transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.8, 1.2)),
+    # transforms.GaussianBlur(kernel_size=1),
     transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    transforms.RandomErasing(p=0.2)
+    # transforms.RandomErasing(p=0.1)
 ])
 
 # data_transforms = transforms.Compose([
--- model.py
+++ model.py
@@ -8,22 +8,22 @@ from transformers import CLIPModel, CLIPProcessor
 nclasses = 500
 
 
-# # class Net(nn.Module):
-# #     def __init__(self):
-# #         super(Net, self).__init__()
-# #         self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
-# #         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-# #         self.conv3 = nn.Conv2d(20, 20, kernel_size=5)
-# #         self.fc1 = nn.Linear(320, 50)
-# #         self.fc2 = nn.Linear(50, nclasses)
+# class Net(nn.Module):
+#     def __init__(self):
+#         super(Net, self).__init__()
+#         self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
+#         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+#         self.conv3 = nn.Conv2d(20, 20, kernel_size=5)
+#         self.fc1 = nn.Linear(320, 50)
+#         self.fc2 = nn.Linear(50, nclasses)
 
-# #     def forward(self, x):
-# #         x = F.relu(F.max_pool2d(self.conv1(x), 2))
-# #         x = F.relu(F.max_pool2d(self.conv2(x), 2))
-# #         x = F.relu(F.max_pool2d(self.conv3(x), 2))
-# #         x = x.view(-1, 320)
-# #         x = F.relu(self.fc1(x))
-# #         return self.fc2(x)
+#     def forward(self, x):
+#         x = F.relu(F.max_pool2d(self.conv1(x), 2))
+#         x = F.relu(F.max_pool2d(self.conv2(x), 2))
+#         x = F.relu(F.max_pool2d(self.conv3(x), 2))
+#         x = x.view(-1, 320)
+#         x = F.relu(self.fc1(x))
+#         return self.fc2(x)
 
 # class Net(nn.Module):
 
@@ -43,41 +43,36 @@ nclasses = 500
 #         x = self.model(x)
 #         return x
 
-class Net(nn.Module):
-    def __init__(self, num_classes=500):
-        super(Net, self).__init__()
-        self.clip_model = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
-        for param in self.clip_model.parameters():
-            param.requires_grad = False
-        for param in self.clip_model.vision_model.encoder.layers[-4:].parameters():
-            param.requires_grad = True
-        image_embed_dim = self.clip_model.visual_projection.in_features
-        # self.classifier = nn.Sequential(
-        #     nn.Linear(image_embed_dim, 1024),
-        #     nn.ReLU(),
-        #     nn.Dropout(0.5),
-        #     nn.Linear(1024, num_classes)
-        # )
-        # self.classifier = nn.Sequential(
-        #     nn.Linear(image_embed_dim, 1024),
-        #     nn.ReLU(),
-        #     nn.Dropout(0.5),
-        #     nn.Linear(1024, num_classes)
-        # )
-        self.classifier = nn.Sequential(
-            nn.Linear(image_embed_dim, 2048),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(2048, 1024),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(1024, num_classes)
-        )
+# class Net(nn.Module):
+#     def __init__(self, num_classes=500):
+#         super(Net, self).__init__()
+#         self.clip_model = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
+#         for param in self.clip_model.parameters():
+#             param.requires_grad = False
+#         for param in self.clip_model.vision_model.encoder.layers[-1:].parameters():
+#             param.requires_grad = True
+#         image_embed_dim = self.clip_model.visual_projection.in_features
+#         self.classifier = nn.Sequential(
+#             nn.Linear(image_embed_dim, 1024),
+#             nn.ReLU(),
+#             nn.Dropout(0.5),
+#             nn.Linear(1024, num_classes)
+#         )
 
-    def forward(self, x):
-        image_features = self.clip_model.vision_model(pixel_values=x).pooler_output
-        logits = self.classifier(image_features)
-        return logits
+#         # self.classifier = nn.Sequential(
+#         #     nn.Linear(image_embed_dim, 2048),
+#         #     nn.ReLU(),
+#         #     nn.Dropout(0.5),
+#         #     nn.Linear(2048, 1024),
+#         #     nn.ReLU(),
+#         #     nn.Dropout(0.5),
+#         #     nn.Linear(1024, num_classes)
+#         # )
+
+#     def forward(self, x):
+#         image_features = self.clip_model.vision_model(pixel_values=x).pooler_output
+#         logits = self.classifier(image_features)
+#         return logits
 
 # import torch
 # import torch.nn as nn
@@ -103,8 +98,30 @@ class Net(nn.Module):
 #         x = self.classifier(image_features)
 #         return x
 
-# import torch.nn as nn
-# import open_clip
+import torch.nn as nn
+import open_clip
+
+class Net(nn.Module):
+    def __init__(self, num_classes=500):
+        super(Net, self).__init__()
+        self.model, _, _ = open_clip.create_model_and_transforms(
+            'ViT-g-14', pretrained='laion2b_s12b_b42k'
+        )
+        for param in self.model.parameters():
+            param.requires_grad = False
+        image_embed_dim = self.model.visual.output_dim
+        self.classifier = nn.Sequential(
+            nn.Linear(image_embed_dim, 1024),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(1024, num_classes)
+        )
+
+    def forward(self, x):
+        image_features = self.model.encode_image(x)
+        logits = self.classifier(image_features)
+        return logits
+
 
 # nclasses = 500
 
